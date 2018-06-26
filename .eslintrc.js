b4194a8049e86634cdcf6873890c7d252e8b4021
zurajacoco <zurajacoco@gmail.com>
2018-06-27 01:43:44 +0900

    chore(eslintrc): eslintの設定の導入

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "google",
+    "parser": "babel-eslint",
+    "plugins": [
+      "react"
+    ],
+    "rules": {
+      "require-jsdoc": 0,
+      "react/jsx-uses-vars": "error",
+      "react/jsx-uses-react": "error"
+    }
+};
